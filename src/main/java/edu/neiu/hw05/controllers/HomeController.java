d11cbbd1918e7934469653e543b08dec7781374d
hassannnny <lhjaved@neiu.edu>
2021-10-02 00:51:21 -0500

    Initial commit

---
+++ src/main/java/edu/neiu/hw05/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package edu.neiu.hw05.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String getHomePage() {
+        String s = "StudySmart";
+        return s;
+    }
+}
+
---
+++ src/test/java/edu/neiu/hw05/Hw05ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neiu.hw05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
